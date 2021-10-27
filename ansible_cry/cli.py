e2a3b6c6e9e88fb8f15a68e07ecd3d2518ce275f
Pierre-Yves Langlois <pierreyves.langlois@gmail.com>
2021-10-27 11:49:12 -0400

    Fixing shell show-params early exit, using home/.vault.pass for password and showing context on demand

--- ansible_cry/cli.py
+++ ansible_cry/cli.py
@@ -4,33 +4,51 @@ import sys
 
 from plumbum import cli
 from ansible.parsing.vault import VaultLib, VaultSecret
+from pathlib import Path
 
 
 class CRY(cli.Application):
     DESCRIPTION = "CRY me a river of encrypted data. Cry cry Cry."
+
+    show_params = cli.Flag(["--show-params"], default=False,
+                           help="Show parameters given to cry and exit.")
     decrypt = cli.Flag(["-d", "--decrypt"], default=False,
                        help="Encrypt by default, add -d to decrypt")
 
+    config_file = cli.SwitchAttr("--config", cli.ExistingFile,
+                                 help="Filename with the vault password.",
+                                 default=f"{Path.home()}/.vault.pass")
+
     def main(self):
+        if self.show_params:
+            print(sys.argv)
+
         if not self.nested_command:
             print("No command given")
             return 1  # error exit code
-        with open('vault.pass', 'r') as pass_file:
-            self.v = VaultLib([(None, VaultSecret(pass_file.read().encode()))])
+        with open(self.config_file, 'r') as pass_file:
+            self.vault_lib = VaultLib([(None, VaultSecret(pass_file.read().encode()))])
 
 
 @CRY.subcommand("file")
 class CRYFile(cli.Application):
+    show_context = cli.Flag(["-c", "--show-context"], default=False,
+                            help="Show the context around the string to decode")
 
-    @cli.positional(cli.ExistingFile)
     def main(self, *files):
         for file in files:
             with open(file, 'r') as f:
                 file_in_str = f.read()
             if self.parent.decrypt:
-                print(decrypt(file_in_str, self.parent.v))
+                values = decrypt(file_in_str, self.parent.vault_lib)
+                for val in values:
+                    if self.show_context:
+                        print(f"{val[0]}\n{val[1]}\n")
+                    else:
+                        print(val[1])
             else:
-                print(encrypt(file_in_str, self.parent.v))
+                value = encrypt(file_in_str, self.parent.vault_lib)
+                print(value)
 
 
 @CRY.subcommand("string")
@@ -45,14 +63,14 @@ class CRYString(cli.Application):
 
         if self.parent.decrypt:
             for s in strings:
-                print(decrypt(s, self.parent.v))
+                print(decrypt(s, self.parent.vault_lib)[1])
         else:
             for s in strings:
-                print(encrypt(s, self.parent.v))
+                print(encrypt(s, self.parent.vault_lib))
 
 
 def decrypt(s: str, v: VaultLib) -> str:
-    decrypted_string = ""
+    decrypted_strings = []
     s += '\n\n'
     regex = r"\$ANSIBLE_VAULT;[0-9.]+;[A-Z0-9]+\n([0-9\sa-z])+^"
     matches = re.finditer(regex, s, re.MULTILINE)
@@ -60,8 +78,10 @@ def decrypt(s: str, v: VaultLib) -> str:
         m = match.group()
         if m:
             m = m.strip().replace(' ', '').replace('\t', '')
-            print(v.decrypt(m).decode())
-    return decrypted_string
+            start = match.start()
+            context = s[:start + 10].split('\n')[-2:][0].strip()
+            decrypted_strings += [(context, f"{v.decrypt(m).decode()}")]
+    return decrypted_strings
 
 
 def encrypt(s: str, v: VaultLib) -> str:
@@ -69,4 +89,8 @@ def encrypt(s: str, v: VaultLib) -> str:
 
 
 if __name__ == "__main__":
-    CRY.run()
+    try:
+        CRY.run()
+    except (Exception, RuntimeError) as e:
+        raise Exception(f"There is an error in your command parameters. Please verify them: '{sys.argv}'. "
+                        f"Upper exception:{e}")
